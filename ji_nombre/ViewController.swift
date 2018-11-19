9710d349f7edad3c0c389577108e17e117eafb7d
jijiedgray <jie.ji@utt.fr>
2018-11-19 22:36:21 +0100

    Initial Commit

---
+++ ji_nombre/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ji_nombre
+//
+//  Created by 季婕 on 19/11/2018.
+//  Copyright © 2018 if26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
